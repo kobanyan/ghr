f6ec22cac97d3a194a1e953ca56915e79830e331
kobanyan <kobanyan@users.noreply.github.com>
2017-01-24 00:40:30 +0900

    Add completions

---
+++ completions/ghr.fish
@@ -0,0 +1,5 @@
+complete -c ghr -x -d "Download archive from github releases" -a "\t"
+complete -c ghr -f -s h -l help -d "Show usage help."
+complete -c ghr -f -s n -l name -d "Save binary as <name>."
+complete -c ghr -f -s r -l repo -d "Github repository <owner/repo>."
+complete -c ghr -f -s v -l version -d "Download target <version>. Use 'latest' if empty."
