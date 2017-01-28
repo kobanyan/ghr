17ecfc86db061c0af57b9272a7902096ad278f5a
kobanyan <kobanyan@users.noreply.github.com>
2017-01-28 16:43:49 +0900

    Skip installing jq test at running on travis

--- test/test.fish
+++ test/test.fish
@@ -11,23 +11,28 @@ function __error -a message
   set errors (math $errors + 1)
 end
 
-function __test -a repo tag name
+function __test -a repo tag name skip_at_running_on_travis
   echo ------
   set tests (math $tests + 1)
-  test -n "$RUN_ON_TRABIS";
+  # skip at running on travis
+  test -n "$RUN_ON_TRABIS" -a -n "$skip_at_running_on_travis";
     and set skips (math $skips + 1);
     and return
+  # resolve binary name
   set -l binary $name
   test -z "$binary"; and set binary (string split "/" $repo)[-1]
+  # check if not installed
   if type $binary >/dev/null 2>&1;
     __error "$binary should not be installed"
     return 1
   end
+  # install
   set -l options $repo
   test -n "$tag"; and set options $options -t $tag
   test -n "$name"; and set options $options -n $name
   echo ghr $options
   ghr $options
+  # check if installed
   if type $binary >/dev/null 2>&1;
     if test -n "$tag";
       if not string match -r ".*$tag.*" (eval $binary --version);
@@ -49,7 +54,7 @@ __test junegunn/fzf-bin 0.16.0
 
 __test junegunn/fzf-bin "" fzf
 
-__test stedolan/jq
+__test stedolan/jq "" "" true
 
 __test motemen/ghq
 
