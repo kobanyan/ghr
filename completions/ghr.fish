95bee369b246e0002b251e0a3061c2493a9958c2
kobanyan <kobanyan@users.noreply.github.com>
2017-01-28 17:07:31 +0900

    Fix usage text

--- completions/ghr.fish
+++ completions/ghr.fish
@@ -1,4 +1,4 @@
-complete -c ghr -x -d "Download archive from github releases" -a "\t"
+complete -c ghr -x -d "Install a binary from GitHub releases" -a "\t"
 complete -c ghr -f -s h -l help -d "Show usage help"
 complete -c ghr -f -s n -l name -d "Save binary as <name>"
 complete -c ghr -f -s t -l tag -d "Download from <tag>"
